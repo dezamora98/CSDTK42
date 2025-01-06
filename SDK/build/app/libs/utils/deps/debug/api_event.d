60292987a97a4b0176d963c58e106226c64e341a
dezamora98 <dezamora98@gmail.com>
2025-01-05 22:58:39 -0500

    update README

---
+++ SDK/build/app/libs/utils/deps/debug/api_event.d
@@ -0,0 +1,108 @@
+C:/CSDTK42/SDK/build/app/libs/utils/obj/debug/api_event.o: \
+ src/api_event.c C:/CSDTK42/SDK/include/api_inc/api_event.h \
+ C:/CSDTK42/SDK/include/std_inc/stdbool.h \
+ C:/CSDTK42/SDK/include/std_inc/stdint.h C:/CSDTK42/SDK/include/api_os.h \
+ C:/CSDTK42/SDK/include/sdk_init.h \
+ C:/CSDTK42/SDK/include/std_inc/string.h \
+ C:/CSDTK42/SDK/include/std_inc/stddef.h \
+ C:/CSDTK42/SDK/include/std_inc/stdint.h \
+ C:/CSDTK42/SDK/include/std_inc/stdbool.h \
+ C:/CSDTK42/SDK/include/std_inc/limits.h \
+ C:/CSDTK42/SDK/include/std_inc/stddef.h \
+ C:/CSDTK42/SDK/include/api_inc/api_inc_fs.h \
+ C:/CSDTK42/SDK/include/api_inc/api_inc_gpio.h \
+ C:/CSDTK42/SDK/include/api_inc/api_inc_uart.h \
+ C:/CSDTK42/SDK/include/api_inc/api_inc_network.h \
+ C:/CSDTK42/SDK/include/api_inc/api_inc_socket.h \
+ C:/CSDTK42/SDK/include/api_inc/api_inc_os.h \
+ C:/CSDTK42/SDK/include/api_inc/api_inc_sim.h \
+ C:/CSDTK42/SDK/include/api_inc/api_inc_sms.h \
+ C:/CSDTK42/SDK/include/api_inc/api_inc_time.h \
+ C:/CSDTK42/SDK/include/api_inc/api_inc_pm.h \
+ C:/CSDTK42/SDK/include/api_inc/api_inc_call.h \
+ C:/CSDTK42/SDK/include/api_inc/api_inc_adc.h \
+ C:/CSDTK42/SDK/include/api_inc/api_inc_mqtt.h \
+ C:/CSDTK42/SDK/include/api_inc/api_inc_charset.h \
+ C:/CSDTK42/SDK/include/api_inc/api_inc_i2c.h \
+ C:/CSDTK42/SDK/include/api_inc/api_inc_ssl.h \
+ C:/CSDTK42/SDK/include/api_inc/api_inc_spi.h \
+ C:/CSDTK42/SDK/include/api_inc/api_inc_fota.h \
+ C:/CSDTK42/SDK/include/api_inc/api_inc_ss.h \
+ C:/CSDTK42/SDK/include/std_inc/stdarg.h \
+ C:/CSDTK42/SDK/include/api_inc/api_inc_gizwits.h \
+ C:/CSDTK42/SDK/include/api_inc/api_inc_audio.h \
+ C:/CSDTK42/SDK/include/api_inc/setjmp_inc.h \
+ C:/CSDTK42/SDK/include/api_inc/api_hal_gouda_inc.h \
+ C:/CSDTK42/SDK/include/api_inc/api_inc_flash.h
+
+C:/CSDTK42/SDK/include/api_inc/api_event.h:
+
+C:/CSDTK42/SDK/include/std_inc/stdbool.h:
+
+C:/CSDTK42/SDK/include/std_inc/stdint.h:
+
+C:/CSDTK42/SDK/include/api_os.h:
+
+C:/CSDTK42/SDK/include/sdk_init.h:
+
+C:/CSDTK42/SDK/include/std_inc/string.h:
+
+C:/CSDTK42/SDK/include/std_inc/stddef.h:
+
+C:/CSDTK42/SDK/include/std_inc/stdint.h:
+
+C:/CSDTK42/SDK/include/std_inc/stdbool.h:
+
+C:/CSDTK42/SDK/include/std_inc/limits.h:
+
+C:/CSDTK42/SDK/include/std_inc/stddef.h:
+
+C:/CSDTK42/SDK/include/api_inc/api_inc_fs.h:
+
+C:/CSDTK42/SDK/include/api_inc/api_inc_gpio.h:
+
+C:/CSDTK42/SDK/include/api_inc/api_inc_uart.h:
+
+C:/CSDTK42/SDK/include/api_inc/api_inc_network.h:
+
+C:/CSDTK42/SDK/include/api_inc/api_inc_socket.h:
+
+C:/CSDTK42/SDK/include/api_inc/api_inc_os.h:
+
+C:/CSDTK42/SDK/include/api_inc/api_inc_sim.h:
+
+C:/CSDTK42/SDK/include/api_inc/api_inc_sms.h:
+
+C:/CSDTK42/SDK/include/api_inc/api_inc_time.h:
+
+C:/CSDTK42/SDK/include/api_inc/api_inc_pm.h:
+
+C:/CSDTK42/SDK/include/api_inc/api_inc_call.h:
+
+C:/CSDTK42/SDK/include/api_inc/api_inc_adc.h:
+
+C:/CSDTK42/SDK/include/api_inc/api_inc_mqtt.h:
+
+C:/CSDTK42/SDK/include/api_inc/api_inc_charset.h:
+
+C:/CSDTK42/SDK/include/api_inc/api_inc_i2c.h:
+
+C:/CSDTK42/SDK/include/api_inc/api_inc_ssl.h:
+
+C:/CSDTK42/SDK/include/api_inc/api_inc_spi.h:
+
+C:/CSDTK42/SDK/include/api_inc/api_inc_fota.h:
+
+C:/CSDTK42/SDK/include/api_inc/api_inc_ss.h:
+
+C:/CSDTK42/SDK/include/std_inc/stdarg.h:
+
+C:/CSDTK42/SDK/include/api_inc/api_inc_gizwits.h:
+
+C:/CSDTK42/SDK/include/api_inc/api_inc_audio.h:
+
+C:/CSDTK42/SDK/include/api_inc/setjmp_inc.h:
+
+C:/CSDTK42/SDK/include/api_inc/api_hal_gouda_inc.h:
+
+C:/CSDTK42/SDK/include/api_inc/api_inc_flash.h:
